ec1822c779b2a19e125ef06e283d0fd99c1bdd1c
PedroHenriqueSS <129234362+Pdhenrique@users.noreply.github.com>
2023-08-24 23:52:16 -0300

    feat: added taskCard component

--- React/src/tasks/task-1/TaskManager.tsx
+++ React/src/tasks/task-1/TaskManager.tsx
@@ -1,18 +1,52 @@
 // import { useState } from "react"
 
-import { StyledContainer } from "./styled"
+import {
+  CardTitle,
+  ContentWrapper,
+  StyledBtn,
+  StyledContainer,
+  StyledForm,
+  StyledInput,
+  StyledTaskCard,
+  TaskTitle,
+} from ".";
 
-export const TaskManager = () => {
-    
-    // const [ tasks, setTasks ] = useState([]) 
-    
-    return (
-        <StyledContainer>
-            <h1>Task Manager</h1>
-            <form>
-                <input type='text' placeholder="task name" />
-                <input type='submit' value='Send'/>
-            </form>
-        </StyledContainer>
-    )
-}
+import { formtypes } from ".";
+
+export const CardTask: React.FC<formtypes> = ({
+  bgColor,
+  content,
+  state,
+  title,
+  ...props
+}) => {
+  return (
+    <StyledTaskCard {...props}>
+      <CardTitle>{title}</CardTitle>
+      <ContentWrapper>{content}</ContentWrapper>
+      <StyledBtn>Submit</StyledBtn>
+    </StyledTaskCard>
+  );
+};
+
+export const TaskManager: React.FC<formtypes> = ({bgColor,content,state,title, ...props}) => {
+  // const [ tasks, setTasks ] = useState([])
+
+  return (
+    <StyledContainer {...props}>
+      <TaskTitle>Task Manager</TaskTitle>
+
+      <StyledForm>
+        <StyledInput type="text" placeholder="task title" />
+        <StyledInput type="text" placeholder="task state" />
+        <StyledBtn type="submit">Submit</StyledBtn>
+      </StyledForm>
+      <CardTask
+        bgColor='#ffff'
+        content='Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry'
+        title={title}
+        state={state}
+      />
+    </StyledContainer>
+  );
+};
