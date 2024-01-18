b43a93062a40a7c0408628e09a8599c60d071ae9
Pedro Santana <pedroschw.c@gmail.com>
2024-01-18 08:02:31 -0300

    feat: js task-6 isPrime

---
+++ JavaScript/task-6/Exercise.js
@@ -0,0 +1,8 @@
+// Exercise.js
+
+function isPrime(number) {
+    // Implemente a verificação de número primo aqui
+}
+
+console.log(isPrime(5));   // Deve retornar true
+console.log(isPrime(10));  // Deve retornar false
---
+++ JavaScript/task-6/isPrime.js
@@ -0,0 +1,14 @@
+
+function isPrime(number) {
+    if(number / 1 === number ){
+        if(number / number === 1){
+            return true
+        }
+
+    }else{
+        return false
+    }
+}
+
+console.log(isPrime(5));   // Deve retornar true
+console.log(isPrime(10));  // Deve retornar false
